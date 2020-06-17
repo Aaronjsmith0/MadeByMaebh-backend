03ac3b109144372b1147e9548e6a0b51c6fdfe2d
Aaron Smith <aaron.smith@sgws.com>
2020-06-16 22:53:50 -0500

    got user signup, signin, and signout working

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const expressValidator = require('express-validator');
 require('dotenv').config();
 //import routes
 const userRoutes = require('./routes/user');
@@ -17,9 +18,10 @@ mongoose.connect(process.env.DATABASE, {
 }).then(() => console.log('DB Connected'));
 
 //middlewares
-app.use(morgan('dev'))
-app.use(bodyParser.json())
-app.use(cookieParser())
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(expressValidator());
 
 //routes middleware
 app.use("/api", userRoutes);
