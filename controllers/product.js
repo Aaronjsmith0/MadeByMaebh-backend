f199514122ba74cc87f807837f1e86b376eb5b7e
Aaron Smith <aaron.smith@sgws.com>
2020-06-18 18:27:54 -0500

    Got product add routes working and validated

---
+++ controllers/product.js
@@ -0,0 +1,51 @@
+const formidable = require('formidable');
+const _ = require('lodash');
+const fs = require('fs');
+const {errorHandler} = require('../helpers/dbErrorHandler');
+
+const Product = require('../models/product');
+
+exports.create = (req, res) => {
+    let form = new formidable.IncomingForm()
+    form.keepExtensions = true
+    form.parse(req, (err, fields, files) => {
+        if(err) {
+            return res.status(400).json({
+                error: 'Image could not be uploaded'
+            })
+        }
+        //check for all fields to be filled
+        const {name, description, price, category, quantity, shipping} = fields
+
+        if(!name || !description || !price || !category || !quantity || !shipping) {
+            return res.status(400).json({
+                error: 'All fields must be filled out'
+            })
+        }
+
+        let product = new Product(fields)
+
+        // 1kb = 1000
+        // 1mb = 1000000
+
+        if(files.photo) {
+            //console.log('Files Photo: ', files.photo)
+            if(files.photo.size > 1000000) {
+                return res.status(400).json({
+                    error: 'Image should be less than 1mb in size'
+                })
+            }
+            product.photo.data = fs.readFileSync(files.photo.path)
+            product.photo.contentType = files.photo.type
+        }
+
+        product.save((err, result) => {
+            if(err) {
+                return res.status(400).json({
+                    error: errorHandler(err)
+                })
+            }
+            res.json(result)
+        })
+    })
+};
