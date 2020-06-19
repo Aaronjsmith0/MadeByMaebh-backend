757a4704a53aacbd3affa4ae9a1d8a7bc91b2baa
Aaron Smith <aaron.smith@sgws.com>
2020-06-18 20:58:38 -0500

    still working on backend

--- controllers/product.js
+++ controllers/product.js
@@ -5,7 +5,80 @@ const {errorHandler} = require('../helpers/dbErrorHandler');
 
 const Product = require('../models/product');
 
+exports.productById = (req, res, next, id) => {
+    Product.findById(id).exec((err, product) => {
+        if(err || !product) {
+            return res.status(400).json({
+                error: 'Product not found'
+            });
+        };
+        req.product = product;
+        next();
+    });
+};
+
+exports.read = (req, res) => {
+    req.product.photo = undefined
+    return res.json(req.product)
+}
+
 exports.create = (req, res) => {
+    let form = new formidable.IncomingForm()
+    form.keepExtensions = true
+    form.parse(req, (err, fields, files) => {
+        if(err) {
+            return res.status(400).json({
+                error: 'Image could not be uploaded'
+            });
+        };
+
+        const {name, description, price, category, quantity, shipping} = fields
+
+        if(!name || !description || !price || !category || !quantity || !shipping) {
+            return res.status(400).json({
+                error: 'All fields must be filled out'
+            });
+        };
+
+        let product = new Product(fields);
+
+        if(files.photo) {
+            //console.log('Files Photo: ', files.photo)
+            if(files.photo.size > 1000000) {
+                return res.status(400).json({
+                    error: 'Image should be less than 1mb in size'
+                });
+            };
+            product.photo.data = fs.readFileSync(files.photo.path)
+            product.photo.contentType = files.photo.type
+        };
+
+        product.save((err, result) => {
+            if(err) {
+                return res.status(400).json({
+                    error: errorHandler(err)
+                })
+            }
+            res.json(result)
+        });
+    });
+};
+
+exports.remove = (req, res) => {
+    let product = req.product
+    product.remove((err) => {
+        if(err) {
+            return res.status(400).json({
+                error: errorHandler(err)
+            })
+        }
+        res.json({
+            "message": 'Product Deleted!'
+        });
+    });
+};
+
+exports.update = (req, res) => {
     let form = new formidable.IncomingForm()
     form.keepExtensions = true
     form.parse(req, (err, fields, files) => {
@@ -14,7 +87,6 @@ exports.create = (req, res) => {
                 error: 'Image could not be uploaded'
             })
         }
-        //check for all fields to be filled
         const {name, description, price, category, quantity, shipping} = fields
 
         if(!name || !description || !price || !category || !quantity || !shipping) {
@@ -23,13 +95,10 @@ exports.create = (req, res) => {
             })
         }
 
-        let product = new Product(fields)
-
-        // 1kb = 1000
-        // 1mb = 1000000
+        let product = req.product
+        product = _.extend(product, fields)
 
         if(files.photo) {
-            //console.log('Files Photo: ', files.photo)
             if(files.photo.size > 1000000) {
                 return res.status(400).json({
                     error: 'Image should be less than 1mb in size'
@@ -46,6 +115,34 @@ exports.create = (req, res) => {
                 })
             }
             res.json(result)
-        })
-    })
+        });
+    });
 };
+
+//sell / arrival
+//by sell = /products?sortBy=sold&order=desc&limit=4
+//by arrival = /products?sortBy=createdAt&order=desc&limit=4
+// if no params are sent then all products are returned
+
+
+exports.list = (req, res) => {
+    let order = req.query.order ? req.query.order : 'asc'
+    let sortBy = req.query.sortBy ? req.query.sortBy : '_id'
+    let limit = req.query.limit ? req.query.limit : 6
+
+    Product.find()
+        .select("-photo")
+        .populate('category')
+        .sort([[sortBy, order]])
+        .limit(limit)
+        .exec((err, products) => {
+            if(err) {
+                return res.status(400).json({
+                    error: 'Products not found'
+                })
+            }
+            res.send(products);
+        });
+};
+
+
