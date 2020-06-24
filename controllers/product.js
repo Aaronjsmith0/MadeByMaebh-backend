2460e019f0338c3fb105435f3576cc5747f63a9f
Aaron Smith <aaron.smith@sgws.com>
2020-06-24 15:01:20 -0500

    Got backend set up now moving to client side

--- controllers/product.js
+++ controllers/product.js
@@ -1,13 +1,15 @@
 const formidable = require('formidable');
 const _ = require('lodash');
 const fs = require('fs');
-const {errorHandler} = require('../helpers/dbErrorHandler');
+const {
+    errorHandler
+} = require('../helpers/dbErrorHandler');
 
 const Product = require('../models/product');
 
 exports.productById = (req, res, next, id) => {
     Product.findById(id).exec((err, product) => {
-        if(err || !product) {
+        if (err || !product) {
             return res.status(400).json({
                 error: 'Product not found'
             });
@@ -26,15 +28,22 @@ exports.create = (req, res) => {
     let form = new formidable.IncomingForm()
     form.keepExtensions = true
     form.parse(req, (err, fields, files) => {
-        if(err) {
+        if (err) {
             return res.status(400).json({
                 error: 'Image could not be uploaded'
             });
         };
 
-        const {name, description, price, category, quantity, shipping} = fields
+        const {
+            name,
+            description,
+            price,
+            category,
+            quantity,
+            shipping
+        } = fields
 
-        if(!name || !description || !price || !category || !quantity || !shipping) {
+        if (!name || !description || !price || !category || !quantity || !shipping) {
             return res.status(400).json({
                 error: 'All fields must be filled out'
             });
@@ -42,9 +51,9 @@ exports.create = (req, res) => {
 
         let product = new Product(fields);
 
-        if(files.photo) {
+        if (files.photo) {
             //console.log('Files Photo: ', files.photo)
-            if(files.photo.size > 1000000) {
+            if (files.photo.size > 1000000) {
                 return res.status(400).json({
                     error: 'Image should be less than 1mb in size'
                 });
@@ -54,7 +63,7 @@ exports.create = (req, res) => {
         };
 
         product.save((err, result) => {
-            if(err) {
+            if (err) {
                 return res.status(400).json({
                     error: errorHandler(err)
                 })
@@ -67,7 +76,7 @@ exports.create = (req, res) => {
 exports.remove = (req, res) => {
     let product = req.product
     product.remove((err) => {
-        if(err) {
+        if (err) {
             return res.status(400).json({
                 error: errorHandler(err)
             })
@@ -82,14 +91,21 @@ exports.update = (req, res) => {
     let form = new formidable.IncomingForm()
     form.keepExtensions = true
     form.parse(req, (err, fields, files) => {
-        if(err) {
+        if (err) {
             return res.status(400).json({
                 error: 'Image could not be uploaded'
             })
         }
-        const {name, description, price, category, quantity, shipping} = fields
+        const {
+            name,
+            description,
+            price,
+            category,
+            quantity,
+            shipping
+        } = fields
 
-        if(!name || !description || !price || !category || !quantity || !shipping) {
+        if (!name || !description || !price || !category || !quantity || !shipping) {
             return res.status(400).json({
                 error: 'All fields must be filled out'
             })
@@ -98,8 +114,8 @@ exports.update = (req, res) => {
         let product = req.product
         product = _.extend(product, fields)
 
-        if(files.photo) {
-            if(files.photo.size > 1000000) {
+        if (files.photo) {
+            if (files.photo.size > 1000000) {
                 return res.status(400).json({
                     error: 'Image should be less than 1mb in size'
                 })
@@ -109,7 +125,7 @@ exports.update = (req, res) => {
         }
 
         product.save((err, result) => {
-            if(err) {
+            if (err) {
                 return res.status(400).json({
                     error: errorHandler(err)
                 })
@@ -126,23 +142,117 @@ exports.update = (req, res) => {
 
 
 exports.list = (req, res) => {
-    let order = req.query.order ? req.query.order : 'asc'
-    let sortBy = req.query.sortBy ? req.query.sortBy : '_id'
-    let limit = req.query.limit ? req.query.limit : 6
+    let order = req.query.order ? req.query.order : 'asc';
+    let sortBy = req.query.sortBy ? req.query.sortBy : '_id';
+    let limit = req.query.limit ? parseInt(req.query.limit) : 6;
 
     Product.find()
         .select("-photo")
         .populate('category')
-        .sort([[sortBy, order]])
+        .sort([
+            [sortBy, order]
+        ])
         .limit(limit)
         .exec((err, products) => {
-            if(err) {
+            if (err) {
                 return res.status(400).json({
                     error: 'Products not found'
                 })
             }
-            res.send(products);
+            res.json(products);
         });
 };
+
+//it will find the products based of the req product category
+//other products that have the same category will be returned
+
+exports.listRelated = (req, res) => {
+    let limit = req.query.limit ? parseInt(req.query.limit) : 6;
+
+    Product.find({
+            _id: {
+                $ne: req.product
+            },
+            category: req.product.category
+        })
+        .limit(limit)
+        .populate('category', '_id name')
+        .exec((err, products) => {
+            if (err) {
+                return res.status(400).json({
+                    error: 'Products not found'
+                })
+            }
+            res.json(products);
+        })
+};
+
+exports.listCategories = (req, res) => {
+    Product.distinct("category", {}, (err, categories) => {
+        if (err) {
+            return res.status(400).json({
+                error: 'Categories not found'
+            })
+        }
+        res.json(categories);
+    });
+};
+
+
+exports.listBySearch = (req, res) => {
+    let order = req.body.order ? req.body.order : "desc";
+    let sortBy = req.body.sortBy ? req.body.sortBy : "_id";
+    let limit = req.body.limit ? parseInt(req.body.limit) : 100;
+    let skip = parseInt(req.body.skip);
+    let findArgs = {};
+
+    // console.log(order, sortBy, limit, skip, req.body.filters);
+    // console.log("findArgs", findArgs);
+
+    for (let key in req.body.filters) {
+        if (req.body.filters[key].length > 0) {
+            if (key === "price") {
+                // gte -  greater than price [0-10]
+                // lte - less than
+                findArgs[key] = {
+                    $gte: req.body.filters[key][0],
+                    $lte: req.body.filters[key][1]
+                };
+            } else {
+                findArgs[key] = req.body.filters[key];
+            }
+        }
+    }
+
+    Product.find(findArgs)
+        .select("-photo")
+        .populate("category")
+        .sort([
+            [sortBy, order]
+        ])
+        .skip(skip)
+        .limit(limit)
+        .exec((err, data) => {
+            if (err) {
+                return res.status(400).json({
+                    error: "Products not found"
+                });
+            }
+            res.json({
+                size: data.length,
+                data
+            });
+        });
+};
+
+exports.photo = (req, res, next) => {
+    if(req.product.photo.data) {
+        res.set('Content-Type', req.product.photo.contentType)
+        return res.send(req.product.photo.data)
+    }
+    next();
+}; 
+
+
 
 
--- routes/product.js
+++ routes/product.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router = express.Router();
 
-const { create, productById, read, remove, update, list } = require('../controllers/product');
-const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
-const { userById } = require('../controllers/user');
+const {
+    create,
+    productById,
+    read,
+    remove,
+    update,
+    list,
+    listRelated,
+    listCategories,
+    listBySearch,
+    photo
+} = require('../controllers/product');
+const {
+    requireSignin,
+    isAuth,
+    isAdmin
+} = require('../controllers/auth');
+const {
+    userById
+} = require('../controllers/user');
 
 router.get('/product/:productId', read)
 router.post('/product/create/:userId', requireSignin, isAuth, isAdmin, create)
@@ -11,8 +28,12 @@ router.delete('/product/:productId/:userId', requireSignin, isAuth, isAdmin, rem
 router.put('/product/:productId/:userId', requireSignin, isAuth, isAdmin, update)
 
 router.get('/products', list)
+router.get('/products/related/:productId', listRelated)
+router.get('/products/categories', listCategories)
+router.post("/products/by/search", listBySearch);
+router.get('/product/photo/:productId', photo)
 
 router.param('userId', userById)
 router.param('productId', productById)
 
-module.exports = router;
+module.exports = router; 
