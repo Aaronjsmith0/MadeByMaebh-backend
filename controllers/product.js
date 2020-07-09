57950b2f58fd8ebe85a14728fb4a2ce0fb871996
Aaron Smith <aaron.smith@sgws.com>
2020-07-09 16:00:40 -0500

    added order and view order for admin

--- controllers/product.js
+++ controllers/product.js
@@ -279,4 +279,21 @@ exports.listSearch = (req, res) => {
     }
 }
 
-
+exports.decreaseQuantity = (req, res) => {
+    let bulkOps = req.body.order.products.map((item) => {
+        return {
+            updateOne: {
+                filter: {_id: item._id},
+                update: {$inc: {quantity: -item.count, sold: +item.count}}
+            }
+        }
+    })
+    Product.bulkWrite(bulkOps, {}, (error, products) => {
+        if(error) {
+            return res.status(400).json({
+                error: 'Could not update product'
+            })
+        }
+        next();
+    })
+}
